c0db302390d2ef487efa4dc513a25ca4194dc5ab
Chris Sawczuk <gummybassist@gmail.com>
2015-12-06 19:42:51 +0000

    Can now answer question 2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ public class Main {
         CalculatorGenerator<WrapperPaperCalculator> generator = new CalculatorGenerator<>(Main.class.getResourceAsStream("input.txt"), (width, length, height) -> new WrapperPaperCalculator(width, length, height));
         final int[] output = new int[1];
         generator.forEachWrappingPaperCalculator(calculator -> output[0] += calculator.slackRequired() + calculator.totalArea());
-        System.out.println("Total required: "+output[0]);
+        System.out.println("Total wrapping paper required: "+output[0]);
+
+        CalculatorGenerator<RibbonCalculator> ribbonGenerator = new CalculatorGenerator<>(Main.class.getResourceAsStream("input.txt"), (width, length, height) -> new RibbonCalculator(width, length, height));
+        final int[] ribbonOutput = new int[1];
+        ribbonGenerator.forEachWrappingPaperCalculator(calculator -> ribbonOutput[0] += calculator.lengthForBow() + calculator.lengthForWrapping());
+        System.out.println("Total ribbon required: "+ribbonOutput[0]);
     }
 }
--- test/RibbonCalculatorTest.java
+++ test/RibbonCalculatorTest.java
@@ -16,23 +16,4 @@ public class RibbonCalculatorTest {
         assertThat(calculator.lengthForBow(), is(24));
     }
 
-    public static class RibbonCalculator {
-        private final int lengthForWrapping;
-        private final int lengthForBow;
-
-        public RibbonCalculator(int width, int length, int height) {
-            this.lengthForWrapping = (2 * width) + (2 * length) + (2*height) - (Math.max(width, Math.max(length, height)) * 2);
-            this.lengthForBow = width * length * height;
-        }
-
-        int lengthForWrapping()
-        {
-            return lengthForWrapping;
-        }
-
-        int lengthForBow()
-        {
-            return lengthForBow;
-        }
-    }
 }
